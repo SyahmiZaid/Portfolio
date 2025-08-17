818075b6235198b5082bd60cef797bd6b2c09a5f
SyahmiZaid <120901404+SyahmiZaid@users.noreply.github.com>
2025-08-17 22:37:34 +0800

    Adds navigation and hero components to home page
    Implements core page structure by integrating navbar and hero section components into the main content area. Establishes foundational layout for the home page user interface.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { ThemeToggle } from "../components/ThemeToggle"
 import { StarBackground } from "@/components/StarBackground"
+import { Navbar } from "../components/NavBar"
+import { HeroSection } from "../components/HeroSection"
 
 export const Home = () => {
   return (
@@ -12,9 +14,12 @@ export const Home = () => {
         <StarBackground />
 
         {/* Navbar */}
+        <Navbar />
 
         {/* Main Content */}
-
+        <main>
+          <HeroSection />
+        </main>
 
         {/* Footer */}
     </div>
